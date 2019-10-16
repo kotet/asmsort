36d76b995e27bcc644d537837a8b6c31e0fb9157
Kotet <kotet0418@gmail.com>
2019-10-16 10:30:54 +0900

    intel -> at&t: hello.s

--- hello.s
+++ hello.s
@@ -1,20 +1,19 @@
-.intel_syntax noprefix
-.global main
+.globl main
 
 main:
-    push rbp
-    mov rbp, rsp
+    push %rbp
+    mov %rsp, %rbp
 mainloop:
     call getint
     call putint
-    dec rsp
-    mov byte ptr [rsp], 0x0A
-    mov rax, rsp
-    mov rbx, 1
+    dec %rsp
+    movb $0x0A, (%rsp)
+    mov %rsp, %rax
+    movq $1, %rbx
     call putstr
     jmp mainloop
 
-    mov rsp, rbp
-    pop rbp
-    mov rax, 0
+    mov %rbp, %rsp
+    pop %rbp
+    mov $0, %rax
     ret
